ed04b2f8174ee39c152590c3969a1391d9230ce4
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-03-08 10:42:23 +0100

    Create the model "User"

--- espace_utc_api/models.py
+++ espace_utc_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    hash = models.CharField(max_length=100)  # Password
+    name = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
