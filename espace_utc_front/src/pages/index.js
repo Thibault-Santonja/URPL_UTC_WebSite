310a9760b38a40cc5ae545a1b2898f32fc15a000
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-04-03 23:05:54 +0200

    Update Readme

---
+++ espace_utc_front/src/pages/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class Index extends Component {
+
+    render() {
+        return(
+            <>
+                <h1>Yop !</h1>
+            </>
+        )
+    };
+
+}
+
+export {Index};
