c305f81685e61e4afff608fb7e56f658b62dd690
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-04-09 09:03:08 +0200

    Add 404 page

---
+++ urpl_front/src/pages/notFound.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+class NotFound extends Component {
+
+    render() {
+        return(
+            <div className={'page'}>
+                <h2 className={'page_title'}>404 : Not Found</h2>
+                <article>
+                    Aie Aie Aie
+                </article>
+            </div>
+        )
+    };
+
+}
+
+export {NotFound};
