683abee298f44a6d5214e83e1c39b4d23387078c
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-04-09 09:02:51 +0200

    Add aboutUs.js

---
+++ urpl_front/src/pages/aboutUs.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+class AboutUs extends Component {
+
+    render() {
+        return(
+            <div className={'page'}>
+                <h2 className={'page_title'}>About us</h2>
+                <article>
+                    Nous nous nous, c'est le gout !
+                </article>
+            </div>
+        )
+    };
+
+}
+
+export {AboutUs};
